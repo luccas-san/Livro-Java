22e42fb1533e3edce34a192fb2aadf1ab950acb6
Lucas <Lucas.sampa095@hotmail.com>
2023-07-04 19:08:01 -0300

    Adicionado Arquivo IML

--- Capitulo_Quatro/src/vehicle5/Vehicle.java
+++ Capitulo_Quatro/src/vehicle5/Vehicle.java
@@ -2,9 +2,7 @@
 * Adiciona um método parametrizado que calcula o combustível
 * necessario para cobrir uma determinada distância.
 * */
-
 package vehicle5;
-
 public class Vehicle {
     int passengers; // número de passageiros
     int fuelcap; // capacidade de armazenamento de combustível em galões
@@ -14,13 +12,11 @@ public class Vehicle {
     int range() {
         return mpg * fuelcap;
     }
-
     // Calcula o combustével necessario para cobrir uma determinada distância
     double fuelneeded(int miles) {
         return (double) miles / mpg;
     }
 }
-
 class CompFuel {
     public static void main(String[] args) {
         Vehicle minivan = new Vehicle();
