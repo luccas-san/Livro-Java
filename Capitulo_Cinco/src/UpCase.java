a26a7056e53bad9e76663866e22cc1aeb97c71b0
Lucas <Lucas.sampa095@hotmail.com>
2023-07-07 22:50:45 -0300

    Last commit Capitulo 5

---
+++ Capitulo_Cinco/src/UpCase.java
@@ -0,0 +1,17 @@
+// Letras maiusculas
+public class UpCase {
+    public static void main(String[] args) {
+
+        char ch;
+
+        for(int i = 0; i < 10; i++) {
+            ch = (char) ('a' + i);
+            System.out.print(ch);
+
+            // Essa instrução desativa o 6° bit
+            ch = (char) ((int) ch & 65503); // agora ch é maiuscula
+
+            System.out.print(ch + " ");
+        }
+    }
+}
